4f880c255fe0b350f7791261e4646d863523f22b
Kat Marchán <kzm@zkat.tech>
2021-10-12 12:11:53 -0700

    fix codes

--- crates/collider-electron/src/errors.rs
+++ crates/collider-electron/src/errors.rs
@@ -8,22 +8,22 @@ use collider_common::{
 #[derive(Debug, Error, Diagnostic)]
 pub enum ElectronError {
     #[error(transparent)]
-    #[diagnostic(code(collider::start::http_error))]
+    #[diagnostic(code(collider::electron::http_error))]
     HttpError(#[from] reqwest::Error),
 
     #[error(transparent)]
-    #[diagnostic(code(collider::start::io_error))]
+    #[diagnostic(code(collider::electron::io_error))]
     IoError(#[from] std::io::Error),
 
     #[error("Failed to get the currently-executing collider binary")]
     #[diagnostic(
-        code(collider::start::current_exe_failure),
+        code(collider::electron::current_exe_failure),
         help("Acquiring the path of the current executable is a platform-specific operation that can fail for a good number of reasons. Some errors can include, but not be limited to, filesystem operations failing or general syscall failures.")
     )]
     CurrentExeFailure(#[source] std::io::Error),
 
     #[error("Found some bad JSON")]
-    #[diagnostic(code(collider::start::bad_package_json))]
+    #[diagnostic(code(collider::electron::bad_package_json))]
     BadJson {
         source: collider_common::serde_json::Error,
         url: String,
@@ -34,59 +34,59 @@ pub enum ElectronError {
     },
 
     #[error(transparent)]
-    #[diagnostic(code(collider::start::zip_error))]
+    #[diagnostic(code(collider::electron::zip_error))]
     ZipError(#[from] zip::result::ZipError),
 
     #[error(transparent)]
-    #[diagnostic(code(collider::start::github_api))]
+    #[diagnostic(code(collider::electron::github_api))]
     GitHubApiError(octocrab::Error),
 
     #[error("{0}")]
     #[diagnostic(
-        code(collider::start::github_api::request_limit),
+        code(collider::electron::github_api::request_limit),
         help("Consider passing in a GitHub API Token using `--github-token`, or using a different one."),
     )]
     GitHubApiLimit(octocrab::GitHubError),
 
     #[error("Could not find matching Electron files for release: {target}.")]
-    #[diagnostic(code(collider::start::missing_electron_files))]
+    #[diagnostic(code(collider::electron::missing_electron_files))]
     MissingElectronFiles {
         version: node_semver::Version,
         target: String,
     },
 
     #[error("A matching electron version could not be found for `electron@{0}`")]
-    #[diagnostic(code(collider::start::matching_version_not_found))]
+    #[diagnostic(code(collider::electron::matching_version_not_found))]
     MatchingVersionNotFound(node_semver::Range),
 
     #[error("Unsupported architecture: {0}.")]
     #[diagnostic(
-        code(collider::start::unsupported_arch),
+        code(collider::electron::unsupported_arch),
         help("Electron only supports ia32, x64, arm64, and arm7l.")
     )]
     UnsupportedArch(String),
 
     #[error("Unsupported platform: {0}.")]
     #[diagnostic(
-        code(collider::start::unsupported_arch),
+        code(collider::electron::unsupported_arch),
         help("Electron only supports win32, linux, and darwin.")
     )]
     UnsupportedPlatform(String),
 
     #[error("Platform-specific project directory could not be determined.")]
-    #[diagnostic(code(collider::start::no_project_dir))]
+    #[diagnostic(code(collider::electron::no_project_dir))]
     NoProjectDir,
 
     #[error(transparent)]
-    #[diagnostic(code(collider::start::semver_error))]
+    #[diagnostic(code(collider::electron::semver_error))]
     SemverError(#[from] node_semver::SemverError),
 
     #[error("Failed to parse package.json")]
-    #[diagnostic(code(collider::start::parse_package_json))]
+    #[diagnostic(code(collider::electron::parse_package_json))]
     ParsePackageJson(#[from] collider_common::serde_json::Error),
 
     #[error("Electron process exited with an error")]
-    #[diagnostic(code(collider::start::electron_error))]
+    #[diagnostic(code(collider::electron::electron_error))]
     ElectronFailed,
 }
 
