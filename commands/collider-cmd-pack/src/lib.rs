9b08f7b0e060f48da16885b20ad09fb4bf5e3120
Kat Marchán <kzm@zkat.tech>
2021-10-14 13:41:43 -0700

    asar building off staging, and copying to destination all working

--- commands/collider-cmd-pack/src/lib.rs
+++ commands/collider-cmd-pack/src/lib.rs
@@ -8,9 +8,11 @@ use collider_command::{
 };
 use collider_common::{
     miette::{self, Context, IntoDiagnostic, Result},
-    smol::{fs, process::Command},
+    smol::{self, fs, process::Command},
 };
 use collider_electron::{Electron, ElectronOpts};
+use flate2::read::GzDecoder;
+use tar::Archive;
 
 #[derive(Debug, Clap, ColliderConfigLayer)]
 pub struct PackCmd {
@@ -61,27 +63,85 @@ impl ColliderCommand for PackCmd {
         let out = self.output.clone();
         // Make sure we've downloaded & cached an electron version
         let electron = self.ensure_electron().await?;
-        let asar = self.ensure_asar(&electron).await?;
         fs::create_dir_all(&out)
             .await
             .into_diagnostic()
             .context("Failed to create output directory")?;
-        let rel_electron = self.prepare_release(&electron, &asar, &out).await?;
+        let (build_dir, rel_electron) = self.ensure_build_dir(&electron, &out).await?;
+        let asar = self.ensure_asar(&rel_electron, &build_dir).await?;
+        self.place_asar(
+            &rel_electron,
+            &asar,
+            &build_dir.join("release").join("resources").join("app.asar"),
+        )
+        .await?;
         println!("{:#?}", rel_electron);
         Ok(())
     }
 }
 
 impl PackCmd {
-    async fn ensure_asar(&self, electron: &Electron) -> Result<PathBuf> {
+    async fn ensure_asar(&self, electron: &Electron, build_dir: &Path) -> Result<PathBuf> {
         if let Some(asar) = &self.asar {
             return Ok(asar.clone());
         }
-        self.prune_proj(&self.path).await?;
-        self.rebuild_proj(&self.path, electron).await?;
-        let asar_dest = self.path.join("node_modules").join("app.asar");
-        self.pack_asar(&self.path, &asar_dest).await?;
+        // TODO: npm pack the project up, extract it into the build dir, `npm
+        // i --production` it, then continue with the rest here.
+        let tarball = self.npm_pack_proj(&self.path).await?;
+        let proj_dest = self.extract_to_build_dir(&tarball, build_dir).await?;
+        self.prune_proj(&proj_dest).await?;
+        self.rebuild_proj(&proj_dest, electron).await?;
+        let asar_dest = build_dir.join("app.asar");
+        self.pack_asar(&proj_dest, &asar_dest).await?;
         Ok(asar_dest)
+    }
+
+    async fn npm_pack_proj(&self, proj_dir: &Path) -> Result<PathBuf> {
+        let npm_path = which::which("npm").into_diagnostic().context(
+            "Failed to find npm command while packaging project. NPM/npx are required by collider.",
+        )?;
+
+        // TODO: pnpm and Yarn support. See https://github.com/zkochan/which-pm. For now, just use NPM :)
+        let mut cmd = if cfg!(target_os = "windows") {
+            let mut cmd = Command::new("cmd");
+            cmd.arg("/c");
+            cmd.arg(npm_path);
+            cmd
+        } else {
+            Command::new(npm_path)
+        };
+
+        let output = cmd
+            .arg("pack")
+            .output()
+            .await
+            .into_diagnostic()
+            .context("Failed to spawn NPM")?;
+
+        if !output.status.success() {
+            miette::bail!("NPM pack failed")
+        }
+
+        let package_file = String::from_utf8(output.stdout)
+            .into_diagnostic()
+            .context("Package name is invalid utf8")?;
+
+        Ok(proj_dir.join(&package_file.trim()))
+    }
+
+    async fn extract_to_build_dir(&self, tarball: &Path, build_dir: &Path) -> Result<PathBuf> {
+        let tarball_clone = tarball.to_owned();
+        let build_dir_clone = build_dir.to_owned();
+        smol::unblock(move || {
+            let mut archive = Archive::new(GzDecoder::new(
+                std::fs::File::open(&tarball_clone).expect("Opening the tarball failed?"),
+            ));
+            archive.unpack(&build_dir_clone)
+        })
+        .await
+        .into_diagnostic()
+        .context("Failed to extract build tarball to staging area")?;
+        Ok(build_dir.join("package"))
     }
 
     async fn ensure_electron(&self) -> Result<Electron> {
@@ -96,12 +156,11 @@ impl PackCmd {
         Ok(electron)
     }
 
-    async fn prepare_release(
+    async fn ensure_build_dir(
         &self,
         electron: &Electron,
-        asar: &Path,
         out: &Path,
-    ) -> Result<Electron> {
+    ) -> Result<(PathBuf, Electron)> {
         let electron_dir = electron
             .exe()
             .parent()
@@ -111,12 +170,20 @@ impl PackCmd {
             .file_name()
             .expect("BUG: This should have a file name.");
         let build_dir = out.join(dirname);
-        let copied_electron = electron.copy_files(&build_dir.join("release")).await?;
-        self.remove_default_app_asar(&copied_electron).await?;
-        let asar_dest = build_dir.join("release").join("resources").join("app.asar");
+        let new_electron = electron.copy_files(&build_dir.join("release")).await?;
+        Ok((build_dir, new_electron))
+    }
+
+    async fn place_asar(&self, electron: &Electron, asar: &Path, dest: &Path) -> Result<()> {
+        self.remove_default_app_asar(electron).await?;
+        tracing::debug!(
+            "Copying .asar from {} to {}",
+            asar.display(),
+            dest.display()
+        );
         let opts = fs_extra::file::CopyOptions::new();
-        fs_extra::file::copy(asar, &asar_dest, &opts).into_diagnostic()?;
-        Ok(copied_electron)
+        fs_extra::file::copy(asar, &dest, &opts).into_diagnostic()?;
+        Ok(())
     }
 
     async fn remove_default_app_asar(&self, electron: &Electron) -> Result<()> {
@@ -200,6 +267,11 @@ impl PackCmd {
     }
 
     async fn pack_asar(&self, proj_dir: &Path, dest: &Path) -> Result<()> {
+        self.run_asar_pack(proj_dir, dest).await?;
+        Ok(())
+    }
+
+    async fn run_asar_pack(&self, proj_dir: &Path, dest: &Path) -> Result<()> {
         tracing::info!("Rebuilding node_modules for target platform.");
         let npx_path = which::which("npx").into_diagnostic().context(
             "Failed to find npx command while packaging project. NPM/npx are required by collider.",
@@ -219,7 +291,7 @@ impl PackCmd {
             .arg("pack")
             .arg(proj_dir)
             .arg(dest)
-            .current_dir(proj_dir)
+            .current_dir(&self.path)
             .status()
             .await
             .into_diagnostic()
