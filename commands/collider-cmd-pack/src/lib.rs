5b94f126592524b74fdb99c286bbd0a590e04c1d
Kat Marchán <kzm@zkat.tech>
2021-10-14 10:49:30 -0700

    checkpoint

--- commands/collider-cmd-pack/src/lib.rs
+++ commands/collider-cmd-pack/src/lib.rs
@@ -15,7 +15,7 @@ use collider_electron::{Electron, ElectronOpts};
 #[derive(Debug, Clap, ColliderConfigLayer)]
 pub struct PackCmd {
     #[clap(
-        about = "Path to Electron app. Must be an index.js file, a folder containing a package.json file, or a folder containing an index.json file, and .html/.htm file. URLs are not supported by this tool.",
+        about = "Path to the root of an Electron app. Must be a directory containing a package.json and any files you want to bundle into the app.",
         default_value = "."
     )]
     path: PathBuf,
@@ -27,6 +27,13 @@ pub struct PackCmd {
         long
     )]
     output: PathBuf,
+
+    #[clap(
+        about = "Path to a prebuilt ASAR file. By default, Collider will build it for you.",
+        short,
+        long
+    )]
+    asar: Option<PathBuf>,
 
     #[clap(long, short, about = "Force download of the Electron binary.")]
     force: bool,
@@ -52,18 +59,27 @@ pub struct PackCmd {
 impl ColliderCommand for PackCmd {
     async fn execute(self) -> Result<()> {
         let out = self.output.clone();
+        // Make sure we've downloaded & cached an electron version
+        let electron = self.ensure_electron().await?;
+        let asar = self.ensure_asar(self.asar.as_deref()).await?;
         fs::create_dir_all(&out)
             .await
             .into_diagnostic()
             .context("Failed to create output directory")?;
 
-        let electron = self.ensure_electron(&out).await?;
         println!("{:#?}", electron);
         Ok(())
     }
 }
 
 impl PackCmd {
+    async fn ensure_asar(&self, asar: Option<&Path>) -> Result<PathBuf> {
+        if let Some(asar) = asar {
+            return Ok(asar);
+        }
+
+    }
+
     async fn ensure_electron(&self, out: &Path) -> Result<Electron> {
         let mut opts = ElectronOpts::new()
             .force(self.force)
@@ -81,19 +97,43 @@ impl PackCmd {
         let dirname = electron_dir
             .file_name()
             .expect("BUG: This should have a file name.");
-        let build_dir = out.join("electron-builds").join(dirname);
+        let build_dir = out.join(dirname);
         let copied_electron = electron.copy_files(&build_dir.join("release")).await?;
-        self.prune_proj(&self.path).await?;
-        self.rebuild_proj(&self.path, &copied_electron).await?;
+        self.remove_default_app_asar(&copied_electron).await?;
+        let asar_dest = build_dir.join("release").join("resources").join("app.asar");
+        if let Some(asar) = &self.asar {
+            // TODO: Copy over .asar
+            let opts = fs_extra::file::CopyOptions::new();
+            fs_extra::file::copy(asar, &asar_dest, &opts).into_diagnostic()?;
+        } else {
+            self.prune_proj(&self.path).await?;
+            self.rebuild_proj(&self.path, &copied_electron).await?;
+            self.pack_asar(&self.path, &asar_dest).await?;
+        }
         Ok(copied_electron)
+    }
+
+    async fn remove_default_app_asar(&self, electron: &Electron) -> Result<()> {
+        let default_app = electron
+            .exe()
+            .parent()
+            .expect("BUG: This should have a parent directory.")
+            .join("resources")
+            .join("default_app.asar");
+        fs::remove_file(&default_app).await.into_diagnostic()?;
+        Ok(())
     }
 
     async fn prune_proj(&self, proj_dir: &Path) -> Result<()> {
         tracing::info!("Pruning current node_modules down to only production dependencies.");
+        // TODO: Instead of doing this, get a direct path to the npm-cli.js
+        // file. This will help bypass the Terminate Batch Job b.s. on
+        // Windows.
         let npm_path = which::which("npm").into_diagnostic().context(
             "Failed to find npm command while packaging project. NPM/npx are required by collider.",
         )?;
 
+        // TODO: pnpm and Yarn support. See https://github.com/zkochan/which-pm. For now, just use NPM :)
         let mut cmd = if cfg!(target_os = "windows") {
             let mut cmd = Command::new("cmd");
             cmd.arg("/c");
@@ -104,10 +144,8 @@ impl PackCmd {
         };
 
         let status = cmd
-            .arg("prune")
-            .arg("--omit")
-            .arg("dev")
-            .arg("--quiet")
+            .arg("install")
+            .arg("--production")
             .current_dir(proj_dir)
             .status()
             .await
@@ -150,6 +188,39 @@ impl PackCmd {
 
         if !status.success() {
             miette::bail!("node_modules rebuild failed.")
+        }
+
+        Ok(())
+    }
+
+    async fn pack_asar(&self, proj_dir: &Path, dest: &Path) -> Result<()> {
+        tracing::info!("Rebuilding node_modules for target platform.");
+        let npx_path = which::which("npx").into_diagnostic().context(
+            "Failed to find npx command while packaging project. NPM/npx are required by collider.",
+        )?;
+
+        let mut cmd = if cfg!(target_os = "windows") {
+            let mut cmd = Command::new("cmd");
+            cmd.arg("/c");
+            cmd.arg(npx_path);
+            cmd
+        } else {
+            Command::new(npx_path)
+        };
+
+        let status = cmd
+            .arg("asar")
+            .arg("pack")
+            .arg(proj_dir)
+            .arg(dest)
+            .current_dir(proj_dir)
+            .status()
+            .await
+            .into_diagnostic()
+            .context("Failed to spawn npx itself.")?;
+
+        if !status.success() {
+            miette::bail!("Packaging up .asar failed.")
         }
 
         Ok(())
